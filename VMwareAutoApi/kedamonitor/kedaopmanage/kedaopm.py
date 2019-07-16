de39de5e76d7a7324b6cd47b5e2e307fb5b8607f
vkhaibao <280764069@qq.com>
2019-07-16 18:06:38 +0800

    first commit

---
+++ VMwareAutoApi/kedamonitor/kedaopmanage/kedaopm.py
@@ -0,0 +1,57 @@
+# encoding: utf-8
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def getalertinfo(opurl, opusername, oppassword):
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    #print(dir(browser))
+
+    browser.get("http://10.2.2.33:8086/Logout.do")
+
+    def is_element_exist(css):
+        s = browser.find_elements_by_id(css)
+        if len(s) == 0:
+            print("元素未找到:%s" %css) 
+            return False
+        elif len(s) == 1:
+            return True
+        else:
+            print("找到%s个元素：%s" %(len(s),css))
+            return False
+
+    Username = browser.find_element_by_name('userName')
+    # 填写前先清空
+    Username.clear()
+    # 模拟系统按键
+    Username.send_keys(opusername)
+
+    Password = browser.find_element_by_name('password')
+    Password.clear()
+    Password.send_keys(oppassword)
+
+    jSubmit = browser.find_element_by_id('btnSubmit')
+    # 模拟点击登录按钮
+    jSubmit.click()
+    browser.get(opurl)
+    xpath = "//table[@class='tableFillBorder']/tbody/tr"
+    tr_num = browser.find_elements_by_xpath(xpath)
+    i = 2
+    devicedict = dict()
+    devicelist = list()
+    while len(tr_num) > i:
+        i = i + 1
+        devname = browser.find_element_by_xpath("//table[@class='tableFillBorder']/tbody/tr[%s]/td[3]/a" %i)
+        devalert = browser.find_element_by_xpath("//table[@class='tableFillBorder']/tbody/tr[%s]/td[4]/a" %i)    
+        devaltime = browser.find_element_by_xpath("//table[@class='tableFillBorder']/tbody/tr[%s]/td[10]" %i)
+        devstatu = browser.find_element_by_xpath("//table[@class='tableFillBorder']/tbody/tr[%s]/td[@class='rowOdd'][6] | //table[@class='tableFillBorder']/tbody/tr[%s]/td[@class='rowEven'][6]" %(i,i))
+        devicedict['hostname'] = devname.text
+        devicedict['alertinfo'] = devalert.text
+        devicedict['alertstatus'] = devstatu.text
+        devicedict['alerttime'] = devaltime.text
+        devicelist.append(devicedict.copy())
+    browser.close()
+    return devicelist
+
--- VMwareAutoApi/kedamonitor/views.py
+++ VMwareAutoApi/kedamonitor/views.py
@@ -3,14 +3,18 @@ from VMwareAPI.models import *
 from django.contrib.sessions.models import Session
 from django.shortcuts import render
 from django.shortcuts import render, redirect, HttpResponse
-from VMwareAPI.views import logincheck
+from VMwareAPI.views import logincheck, authcheck
+from kedamonitor.kedaopmanage import kedaopm
 # Create your views here.
 
 @logincheck
 def index(request):
-    listobj = VMVirtual.objects.all()
-    vmdict = {"vmlist": listobj, "username": request.user}
-    return render(request, 'kedamontemp/index.html', vmdict)
+    url = "http://10.2.2.33:8086/fault/AlarmView.do?viewId=ActiveAlarms"
+    username = "admin"
+    password = "admin"
+    alertinfo = kedaopm.getalertinfo(url, username, password)
+    opalerts = {"alertinfo": alertinfo, "username": request.user}
+    return render(request, 'kedamontemp/index.html', opalerts)
 
 @logincheck
 def accountm(request):
