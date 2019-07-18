265c78f517f978c9fc3c7b7d62cee7d1da4ef96b
vkhaibao <280764069@qq.com>
2019-07-18 10:21:58 +0800

    first commit

---
+++ VMwareAutoApi/VMwareAPI/static/js/progress.js
@@ -0,0 +1,7 @@
+var t_id = setInterval(animate,20);
+function remove_loading() {
+this.clearInterval(t_id);
+var targelem = document.getElementById('loader_container');
+targelem.style.display='none';
+targelem.style.visibility='hidden';
+}
